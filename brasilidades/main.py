60fc7f1053251456899ce8482c854586467dead1
Carlos Eduardo <caduarruda49@gmail.como>
2022-01-29 20:10:57 -0300

    dia28

---
+++ brasilidades/main.py
@@ -0,0 +1,11 @@
+from Cpf_Cnpj import Documento
+from Telefone import Telefone
+
+telefone = '5562998651575'
+telefone_obj = Telefone(telefone)
+print(telefone_obj)
+# cnpj_ex = '35379838000112'
+# cpf_ex = '07324500129'
+
+# doc = Documento.cria_documento(cpf_ex)
+# print(doc)
